773573d633ad6bcc1c913e804f4020f8a6b62073
Elie Andraos <elieandraos31@gmail.com>
2021-12-21 17:10:26 +0200

    :sparkles: vue composable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-export const welcome = () => {
-    return "You're all set! Everything is configured for you, happy package development  🎉 🎉"
+import { computed, ref } from 'vue'
+
+const useClockwork = () => {
+    const $errors = ref([])
+
+    const getErrors = computed(() => (key) => {
+        return $errors.value
+            .filter(({ dataKey }) => dataKey === key)
+            .map(({ message }) => message)
+    })
+
+    const hasErrors = computed(() => (key) => {
+        return !!getErrors.value(key).length
+    })
+
+    const getFirstError = (dataKey) => {
+        return hasErrors.value(dataKey) ? getErrors.value(dataKey)[0] : null
+    }
+
+    return {
+        $errors,
+        hasErrors,
+        getFirstError,
+    }
+}
+
+export default {
+    useClockwork,
 }
