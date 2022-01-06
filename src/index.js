e307b441dab1fd2f45a455b19350271bf5f8cc71
Elie Andraos <elieandraos31@gmail.com>
2022-01-06 18:10:52 +0200

    :fire: remove library export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import { computed, ref } from 'vue'
-import { default as library } from "@elieandraos/clockwork"
-
-export const Clockwork = library
 
 export const useClockwork = () => {
     const $errors = ref([])
